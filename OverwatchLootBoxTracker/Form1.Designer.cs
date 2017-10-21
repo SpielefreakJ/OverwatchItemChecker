3f15fc7a7cc6ba54be04f70dc44ca47a2129eeeb
SpielefreakJ <spielefreakjlp@outlook.de>
2017-10-21 10:10:09 +0200

    Changes Location and Size based on the Windows Skaling :shrug:

--- OverwatchLootBoxTracker/Form1.Designer.cs
+++ OverwatchLootBoxTracker/Form1.Designer.cs
@@ -47,16 +47,16 @@
             this.lblMoreInfoCost2 = new System.Windows.Forms.Label();
             this.lblMoreInfoCost = new System.Windows.Forms.Label();
             this.gBLang.SuspendLayout();
+            this.gbAll.SuspendLayout();
             this.gBMoreCost.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblKosten
             // 
             this.lblKosten.AutoSize = true;
-            this.lblKosten.Location = new System.Drawing.Point(17, 16);
-            this.lblKosten.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblKosten.Location = new System.Drawing.Point(13, 13);
             this.lblKosten.Name = "lblKosten";
-            this.lblKosten.Size = new System.Drawing.Size(66, 17);
+            this.lblKosten.Size = new System.Drawing.Size(50, 13);
             this.lblKosten.TabIndex = 0;
             this.lblKosten.Text = "lblKosten";
             // 
@@ -64,11 +64,9 @@
             // 
             this.gBLang.Controls.Add(this.btnLangEnglish);
             this.gBLang.Controls.Add(this.btnLangGerman);
-            this.gBLang.Location = new System.Drawing.Point(1141, 833);
-            this.gBLang.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gBLang.Location = new System.Drawing.Point(856, 677);
             this.gBLang.Name = "gBLang";
-            this.gBLang.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.gBLang.Size = new System.Drawing.Size(267, 123);
+            this.gBLang.Size = new System.Drawing.Size(200, 100);
             this.gBLang.TabIndex = 1;
             this.gBLang.TabStop = false;
             this.gBLang.Text = "Sprache Ändern";
@@ -76,10 +74,9 @@
             // 
             // btnLangEnglish
             // 
-            this.btnLangEnglish.Location = new System.Drawing.Point(9, 60);
-            this.btnLangEnglish.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLangEnglish.Location = new System.Drawing.Point(7, 49);
             this.btnLangEnglish.Name = "btnLangEnglish";
-            this.btnLangEnglish.Size = new System.Drawing.Size(249, 28);
+            this.btnLangEnglish.Size = new System.Drawing.Size(187, 23);
             this.btnLangEnglish.TabIndex = 1;
             this.btnLangEnglish.Text = "English";
             this.btnLangEnglish.UseVisualStyleBackColor = true;
@@ -87,10 +84,9 @@
             // 
             // btnLangGerman
             // 
-            this.btnLangGerman.Location = new System.Drawing.Point(9, 25);
-            this.btnLangGerman.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLangGerman.Location = new System.Drawing.Point(7, 20);
             this.btnLangGerman.Name = "btnLangGerman";
-            this.btnLangGerman.Size = new System.Drawing.Size(249, 28);
+            this.btnLangGerman.Size = new System.Drawing.Size(187, 23);
             this.btnLangGerman.TabIndex = 0;
             this.btnLangGerman.Text = "German (Deutsch)";
             this.btnLangGerman.UseVisualStyleBackColor = true;
@@ -98,10 +94,9 @@
             // 
             // btnSettings
             // 
-            this.btnSettings.Location = new System.Drawing.Point(43, 833);
-            this.btnSettings.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSettings.Location = new System.Drawing.Point(32, 677);
             this.btnSettings.Name = "btnSettings";
-            this.btnSettings.Size = new System.Drawing.Size(112, 28);
+            this.btnSettings.Size = new System.Drawing.Size(84, 23);
             this.btnSettings.TabIndex = 2;
             this.btnSettings.Text = "Einstellungen";
             this.btnSettings.UseVisualStyleBackColor = true;
@@ -109,10 +104,9 @@
             // 
             // btnSkins
             // 
-            this.btnSkins.Location = new System.Drawing.Point(21, 36);
-            this.btnSkins.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSkins.Location = new System.Drawing.Point(16, 29);
             this.btnSkins.Name = "btnSkins";
-            this.btnSkins.Size = new System.Drawing.Size(133, 92);
+            this.btnSkins.Size = new System.Drawing.Size(100, 75);
             this.btnSkins.TabIndex = 3;
             this.btnSkins.Text = "Skins";
             this.btnSkins.UseVisualStyleBackColor = true;
@@ -120,10 +114,9 @@
             // 
             // btnEmotes
             // 
-            this.btnEmotes.Location = new System.Drawing.Point(21, 135);
-            this.btnEmotes.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnEmotes.Location = new System.Drawing.Point(16, 110);
             this.btnEmotes.Name = "btnEmotes";
-            this.btnEmotes.Size = new System.Drawing.Size(133, 92);
+            this.btnEmotes.Size = new System.Drawing.Size(100, 75);
             this.btnEmotes.TabIndex = 4;
             this.btnEmotes.Text = "Emotes";
             this.btnEmotes.UseVisualStyleBackColor = true;
@@ -131,10 +124,9 @@
             // 
             // btnVictoryPoses
             // 
-            this.btnVictoryPoses.Location = new System.Drawing.Point(21, 235);
-            this.btnVictoryPoses.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnVictoryPoses.Location = new System.Drawing.Point(16, 191);
             this.btnVictoryPoses.Name = "btnVictoryPoses";
-            this.btnVictoryPoses.Size = new System.Drawing.Size(133, 92);
+            this.btnVictoryPoses.Size = new System.Drawing.Size(100, 75);
             this.btnVictoryPoses.TabIndex = 5;
             this.btnVictoryPoses.Text = "Vicoty Poses";
             this.btnVictoryPoses.UseVisualStyleBackColor = true;
@@ -142,10 +134,9 @@
             // 
             // btnVoiceLines
             // 
-            this.btnVoiceLines.Location = new System.Drawing.Point(21, 335);
-            this.btnVoiceLines.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnVoiceLines.Location = new System.Drawing.Point(16, 272);
             this.btnVoiceLines.Name = "btnVoiceLines";
-            this.btnVoiceLines.Size = new System.Drawing.Size(133, 92);
+            this.btnVoiceLines.Size = new System.Drawing.Size(100, 75);
             this.btnVoiceLines.TabIndex = 6;
             this.btnVoiceLines.Text = "Voice Lines";
             this.btnVoiceLines.UseVisualStyleBackColor = true;
@@ -153,10 +144,9 @@
             // 
             // btnSprays
             // 
-            this.btnSprays.Location = new System.Drawing.Point(21, 434);
-            this.btnSprays.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSprays.Location = new System.Drawing.Point(16, 353);
             this.btnSprays.Name = "btnSprays";
-            this.btnSprays.Size = new System.Drawing.Size(133, 92);
+            this.btnSprays.Size = new System.Drawing.Size(100, 75);
             this.btnSprays.TabIndex = 7;
             this.btnSprays.Text = "Sprays";
             this.btnSprays.UseVisualStyleBackColor = true;
@@ -164,10 +154,9 @@
             // 
             // btnHighlightIntros
             // 
-            this.btnHighlightIntros.Location = new System.Drawing.Point(21, 534);
-            this.btnHighlightIntros.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnHighlightIntros.Location = new System.Drawing.Point(16, 434);
             this.btnHighlightIntros.Name = "btnHighlightIntros";
-            this.btnHighlightIntros.Size = new System.Drawing.Size(133, 92);
+            this.btnHighlightIntros.Size = new System.Drawing.Size(100, 75);
             this.btnHighlightIntros.TabIndex = 8;
             this.btnHighlightIntros.Text = "Highlight Intros";
             this.btnHighlightIntros.UseVisualStyleBackColor = true;
@@ -175,10 +164,9 @@
             // 
             // btnWeapons
             // 
-            this.btnWeapons.Location = new System.Drawing.Point(21, 634);
-            this.btnWeapons.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnWeapons.Location = new System.Drawing.Point(16, 515);
             this.btnWeapons.Name = "btnWeapons";
-            this.btnWeapons.Size = new System.Drawing.Size(133, 92);
+            this.btnWeapons.Size = new System.Drawing.Size(100, 75);
             this.btnWeapons.TabIndex = 10;
             this.btnWeapons.Text = "Weapons";
             this.btnWeapons.UseVisualStyleBackColor = true;
@@ -186,10 +174,9 @@
             // 
             // btnPlayerIcons
             // 
-            this.btnPlayerIcons.Location = new System.Drawing.Point(21, 734);
-            this.btnPlayerIcons.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPlayerIcons.Location = new System.Drawing.Point(16, 596);
             this.btnPlayerIcons.Name = "btnPlayerIcons";
-            this.btnPlayerIcons.Size = new System.Drawing.Size(133, 92);
+            this.btnPlayerIcons.Size = new System.Drawing.Size(100, 75);
             this.btnPlayerIcons.TabIndex = 11;
             this.btnPlayerIcons.Text = "Player Icons";
             this.btnPlayerIcons.UseVisualStyleBackColor = true;
@@ -197,22 +184,20 @@
             // 
             // gbAll
             // 
-            this.gbAll.Location = new System.Drawing.Point(163, 36);
-            this.gbAll.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbAll.Controls.Add(this.gBMoreCost);
+            this.gbAll.Location = new System.Drawing.Point(122, 29);
             this.gbAll.Name = "gbAll";
-            this.gbAll.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.gbAll.Size = new System.Drawing.Size(1016, 826);
+            this.gbAll.Size = new System.Drawing.Size(762, 671);
             this.gbAll.TabIndex = 12;
             this.gbAll.TabStop = false;
-            this.gbAll.Text = "Skins";
+            this.gbAll.Text = "gbAll";
             this.gbAll.Visible = false;
             // 
             // btnMoreCost
             // 
-            this.btnMoreCost.Location = new System.Drawing.Point(948, 10);
-            this.btnMoreCost.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnMoreCost.Location = new System.Drawing.Point(711, 8);
             this.btnMoreCost.Name = "btnMoreCost";
-            this.btnMoreCost.Size = new System.Drawing.Size(223, 28);
+            this.btnMoreCost.Size = new System.Drawing.Size(167, 23);
             this.btnMoreCost.TabIndex = 13;
             this.btnMoreCost.Text = "btnMoreCost";
             this.btnMoreCost.UseVisualStyleBackColor = true;
@@ -222,11 +207,9 @@
             // 
             this.gBMoreCost.Controls.Add(this.lblMoreInfoCost2);
             this.gBMoreCost.Controls.Add(this.lblMoreInfoCost);
-            this.gBMoreCost.Location = new System.Drawing.Point(21, 36);
-            this.gBMoreCost.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gBMoreCost.Location = new System.Drawing.Point(410, 258);
             this.gBMoreCost.Name = "gBMoreCost";
-            this.gBMoreCost.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.gBMoreCost.Size = new System.Drawing.Size(1157, 826);
+            this.gBMoreCost.Size = new System.Drawing.Size(868, 671);
             this.gBMoreCost.TabIndex = 0;
             this.gBMoreCost.TabStop = false;
             this.gBMoreCost.Text = "gBMoreCost";
@@ -235,10 +218,9 @@
             // lblMoreInfoCost2
             // 
             this.lblMoreInfoCost2.AutoSize = true;
-            this.lblMoreInfoCost2.Location = new System.Drawing.Point(877, 76);
-            this.lblMoreInfoCost2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMoreInfoCost2.Location = new System.Drawing.Point(658, 62);
             this.lblMoreInfoCost2.Name = "lblMoreInfoCost2";
-            this.lblMoreInfoCost2.Size = new System.Drawing.Size(113, 17);
+            this.lblMoreInfoCost2.Size = new System.Drawing.Size(86, 13);
             this.lblMoreInfoCost2.TabIndex = 1;
             this.lblMoreInfoCost2.Text = "lblMoreInfoCost2";
             this.lblMoreInfoCost2.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -246,20 +228,18 @@
             // lblMoreInfoCost
             // 
             this.lblMoreInfoCost.AutoSize = true;
-            this.lblMoreInfoCost.Location = new System.Drawing.Point(150, 76);
-            this.lblMoreInfoCost.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMoreInfoCost.Location = new System.Drawing.Point(112, 62);
             this.lblMoreInfoCost.Name = "lblMoreInfoCost";
-            this.lblMoreInfoCost.Size = new System.Drawing.Size(105, 17);
+            this.lblMoreInfoCost.Size = new System.Drawing.Size(80, 13);
             this.lblMoreInfoCost.TabIndex = 0;
             this.lblMoreInfoCost.Text = "lblMoreInfoCost";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DodgerBlue;
-            this.ClientSize = new System.Drawing.Size(1195, 876);
-            this.Controls.Add(this.gBMoreCost);
+            this.ClientSize = new System.Drawing.Size(896, 712);
             this.Controls.Add(this.btnSettings);
             this.Controls.Add(this.btnMoreCost);
             this.Controls.Add(this.gBLang);
@@ -274,13 +254,13 @@
             this.Controls.Add(this.btnSkins);
             this.Controls.Add(this.lblKosten);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.gBLang.ResumeLayout(false);
+            this.gbAll.ResumeLayout(false);
             this.gBMoreCost.ResumeLayout(false);
             this.gBMoreCost.PerformLayout();
             this.ResumeLayout(false);
